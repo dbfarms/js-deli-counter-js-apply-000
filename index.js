529a7179d95f899965295a756a283d280c1026c7
Matthew Hunger <matt.hunger@gmail.com>
2017-09-21 11:07:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  linestring = "The line is currently: "
+  var linestring = "The line is currently: "
   if (katzDeliLine === 0) {
     return "The line is currently empty."
   } else {
