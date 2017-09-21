f5462616250e8461f7d2b3967b39e22954ae7320
Matthew Hunger <matt.hunger@gmail.com>
2017-09-21 10:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var katzdeli = []
 
 function takeANumber(katzDeliLine, newName) {
   katzDeliLine.push(newName);
-  return 
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    if (katzDeliLine[i] === newName) {
+      return `Welcome, ${newName}. You are number ${katzDeliLine[i]+1} in line.`
+    }
+  }
 }
